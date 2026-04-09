0fb7993d48c579d62ff99a1a4b9035e229778587
Muhammad Ismail <144256138+Ismail836@users.noreply.github.com>
2026-04-09 20:34:04 +0500

    Merge k Sorted Lists

---
+++ Merge k Sorted Lists.java
@@ -0,0 +1,32 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode root : lists) {
+            if (root != null) {
+                minHeap.add(root);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll();
+            current.next = smallest;
+            current = current.next;
+
+            if (smallest.next != null) {
+                minHeap.add(smallest.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
